6337a231eb3cd524803f21176b528d3335192497
ricardo.121072 <rlalmeida@raiadrogasil.com.br>
2018-01-10 10:44:48 -0200

    initial dev

--- src/main/java/com/wallethub/parser/helper/ParserLog.java
+++ src/main/java/com/wallethub/parser/helper/ParserLog.java
@@ -1,10 +1,50 @@
 package com.wallethub.parser.helper;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParserLog {
+
+    private final String[] args;
+
+    private Map<String, String> maps;
 
     public ParserLog(String[] args) {
         if (args == null) {
             throw new IllegalArgumentException();
         }
+        if (args.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        this.args = args;
+        initMap();
+    }
+
+    protected void initMap() {
+        maps = new HashMap<String, String>();
+        String key;
+        String value;
+
+        for (String arg : args) {
+            putArgsInMaps(arg);
+        }
+
+    }
+
+    protected void putArgsInMaps(String arg) {
+        String[] split = arg.split("=");
+        if (split.length == 2) {
+            String key = split[0].replace("--", "");
+            String value = split[1];
+            maps.put(key, value);
+        }
+    }
+
+    public String[] getArgs() {
+        return args;
+    }
+
+    public Map<String, String> getMaps() {
+        return maps;
     }
 }
--- src/test/java/com/wallethub/parser/helper/ParserLogTest.java
+++ src/test/java/com/wallethub/parser/helper/ParserLogTest.java
@@ -1,11 +1,28 @@
 package com.wallethub.parser.helper;
 
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
 public class ParserLogTest {
+
+    private String[] args;
+    private ParserLog parserLog;
+
+    @Before
+    public void setUp() throws Exception {
+        args = new String[]{
+                "--accessLog=C:/apps/Git/path/to/file",
+                "--startDate=2017-01-01.13:00:00",
+                "--duration=hourly",
+                "--threshold=100"
+        };
+        parserLog = new ParserLog(args);
+    }
 
     @Test
     public void newParserLog_withoutParameters_returnError() {
@@ -16,5 +33,41 @@ public class ParserLogTest {
             assertThat(e, Matchers.instanceOf(IllegalArgumentException.class));
         }
 
+    }
+
+    @Test
+    public void newParserLog_withParametersEmpty_returnError() {
+        try {
+            new ParserLog(new String[]{});
+            fail();
+        } catch (Exception e) {
+            assertThat(e, Matchers.instanceOf(IllegalArgumentException.class));
+        }
+    }
+
+    @Test
+    public void newParserLog_withParameters_setArgs() {
+        assertThat(parserLog.getArgs().length, Matchers.equalTo(4));
+    }
+
+    @Test
+    public void newParserLog_withParameters_xxxx() {
+        parserLog.initMap();
+        String accessLogValue = parserLog.getMaps().get("accessLog");
+        assertThat(accessLogValue, Matchers.equalTo("C:/apps/Git/path/to/file"));
+    }
+
+    @Test
+    public void putArgsInMaps_withStringFull_putInMaps() {
+        parserLog.putArgsInMaps("--accessLog=C:/apps/Git/path/to/file");
+        String value = parserLog.getMaps().get("accessLog");
+        assertThat(value, Matchers.equalTo("C:/apps/Git/path/to/file"));
+    }
+
+    @Test
+    public void putArgsInMaps_withParametersInvalid_notInputMap() {
+        parserLog.putArgsInMaps("=");
+        Map<String, String> maps = parserLog.getMaps();
+        assertThat(maps.size(), Matchers.equalTo(0));
     }
 }
